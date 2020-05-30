052420090fb9c858761263c6228d4c44d69b38ff
Priyanshu Dasgupta <62264051+priyanshudasgupta8@users.noreply.github.com>
2020-05-30 11:19:45 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ class Calculator {
         case '-':
           computation = prev - current
           break
-        case '×':
+        case '*':
           computation = prev * current
           break
         case '÷':
